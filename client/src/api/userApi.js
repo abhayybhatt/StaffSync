19f2ec50135b4488a9c91ea39ac6f99ea95a3d62
abhayybhatt <abhayy.bhatt.w@gmail.com>
2026-01-05 05:18:25 +0530

    update userApi.js

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -1,59 +1,60 @@
-import { LucideMonitor } from "lucide-react";
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1/users";
-
-//get users with pagination
+// get users with pagination
 export const getUsers = async (page = 1, limit = 5) => {
-  const res = await fetch(`${API_URL}?page=${page}&limit=${limit}`);
-  if (!res.ok) throw new Error("Failer to fetch users");
+  const res = await fetch(
+    `${API_URL}/users?page=${page}&limit=${limit}`
+  );
+  if (!res.ok) throw new Error("Failed to fetch users");
   return res.json();
 };
 
-//search users
+// search users
 export const searchUsers = async (term = "", page = 1, limit = 5) => {
   const res = await fetch(
-    `${API_URL}/search/${encodeURIComponent(term)}?page=${page}&limit=${limit}`
+    `${API_URL}/users/search/${encodeURIComponent(
+      term
+    )}?page=${page}&limit=${limit}`
   );
-  if (!res.ok) throw new Error("Failer to search users");
+  if (!res.ok) throw new Error("Failed to search users");
   return res.json();
 };
 
-//get status
+// get status
 export const getStatus = async () => {
-  const res = await fetch(`${API_URL}/status`);
+  const res = await fetch(`${API_URL}/users/status`);
   if (!res.ok) throw new Error("Failed to fetch status");
   return res.json();
 };
 
-//add user
+// add user
 export const addUser = async (data) => {
-  const res = await fetch(API_URL, {
+  const res = await fetch(`${API_URL}/users`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
   if (!res.ok) throw new Error("Failed to add user");
   return res.json();
 };
 
-//update existing user
+// update user
 export const updateUser = async (id, data) => {
-  const res = await fetch(`${API_URL}/${id}`, {
+  const res = await fetch(`${API_URL}/users/${id}`, {
     method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
   if (!res.ok) throw new Error("Failed to update user");
   return res.json();
 };
 
-//delete user
+// delete user
 export const deleteUser = async (id) => {
-  const res = await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+  const res = await fetch(`${API_URL}/users/${id}`, {
+    method: "DELETE",
+  });
   if (!res.ok) throw new Error("Failed to delete user");
   return res.json();
 };
